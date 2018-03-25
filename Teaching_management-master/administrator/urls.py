b1522538293d0e4fdba5b7e2b18782677f7b8da0
梁杰 <313827758@qq.com>
2018-03-25 19:49:04 +0800

    Update urls.py

--- Teaching_management-master/administrator/urls.py
+++ Teaching_management-master/administrator/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^administrator_courses/(.+)/$',views.courses, name='courses'),
     url(r'^logout', views.logout, name='logout'),
+    url(r'^liuyan',views.liuyan),
+    url(r'^deleteliuyan/(.+)/$',views.deleteliuyan)
 ]
